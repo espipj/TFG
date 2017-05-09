08a1f741ce9e992c98be49b86bb2a2f4b5b8ee45
Pablo Espinosa <espipj@gmail.com>
2017-05-09 21:17:07 +0200

    added view for muestras/table

--- resources/views/muestra/tablaMuestras.blade.php
+++ resources/views/muestra/tablaMuestras.blade.php
@@ -1,46 +1,35 @@
-<input type="text" id="buscadorGanados" class="buscadorjs noform" data-tabla="tablaGanados"
-       placeholder="Buscar res por crotal...">
+<input type="text" id="buscadorMuestras" class="buscadorjs noform" data-tabla="tablaMuestras"
+       placeholder="Buscar muestra por NºTubo...">
 
-<label class="checkbox-inline"><input type="checkbox" rel="vivo" value="1" class="tick" data-tabla="tablaGanados">Vivas</label>
-<label class="checkbox-inline"><input type="checkbox" rel="vivo" value="0" class="tick" data-tabla="tablaGanados">Muertas</label>
-<table id="tablaGanados" class="table header-fixed ganados">
+<table id="tablaMuestras" class="table header-fixed ganados">
     <thead>
     <tr class="header">
-        <th style="width:12%;">Crotal</th>
-        <th style="width:15%;">Sexo</th>
-        <th style="width:20%;">Fecha de Nacimiento</th>
-        <th style="width:25%;">Ganadería</th>
+        <th style="width:12%;">Nº Tubo</th>
+        <th style="width:15%;">Extracción</th>
+        <th style="width:20%;">Consulta</th>
+        <th style="width:25%;">Res</th>
         <th style="width:28%;">Acciones</th>
     </tr>
     </thead>
     <tbody>
-    @foreach($ganados as $ganado)
-        @if($ganado->crotal && $ganado->sexo && $ganado->fecha_nacimiento)
+    @foreach($muestras as $muestra)
+        @if($muestra->tubo && $muestra->fecha_extraccion && $muestra->ganado && $muestra->tipoConsulta)
 
 
 
-            <tr class="clickable-row vacas" data-href="{{route('verganado',[$ganado])}}" data-id="{{$ganado->id}}">
-                <td style="width:12%;" class="vivo" rel="{{$ganado->vivo}}">{{$ganado->crotal}}</td>
-                <td style="width:15%;">{{$ganado->sexo->nombre}}</td>
-                <td style="width:20%;">{{$ganado->fecha_nacimiento->format('d-m-Y')}}</td>
-                @if(!empty($ganado->ganaderia))
-                <td style="width:25%;">{{$ganado->ganaderia->nombre}}</td>
-                @else
-                    <td style="width:25%;">No definida</td>
-                @endif
+            <tr class="clickable-row" data-href="{{route('vermuestra',[$muestra])}}" data-id="{{$muestra->id}}">
+                <td style="width:12%;">{{$muestra->tubo}}</td>
+                <td style="width:15%;">{{$muestra->fecha_extraccion->format('d-m-Y')}}</td>
+                <td style="width:20%;">{{$muestra->tipoConsulta->nombre}}</td>
+                <td style="width:25%;">{{$muestra->ganado->crotal}}</td>
                 <td style="width:28%;">
                     <div class="btn-group">
-                        <a href="{{url('ver/ganado',['ganado'=>$ganado])}}" class="btn btn-info btn-sm"
+                        <a href="{{route('vermuestra',[$muestra])}}" class="btn btn-info btn-sm"
                            role="button"><span class="glyphicon glyphicon-list"></span> Detalles</a>
-                        <a href="{{url('editar/ganado',['ganado'=>$ganado])}}" class="btn btn-success btn-sm"
+                        <a href="{{url('editar/muestra',['$muestra'=>$muestra])}}" class="btn btn-success btn-sm"
                            role="button"><span class="glyphicon glyphicon-edit"></span> Editar</a>
-                        @if($ganado->vivo==1)
                         <a href="#!" class="btn btn-danger btn-sm eliminar"
                            role="button"><span class="glyphicon glyphicon-remove"></span> Eliminar</a>
-                        @else
-                            <a href="#!" class="btn btn-danger btn-sm eliminar disabled"
-                               role="button"><span class="glyphicon glyphicon-remove"></span> Eliminar</a>
-                        @endif
                     </div>
                 </td>
             </tr>
@@ -49,7 +38,7 @@
     </tbody>
 </table>
 
-{!! Form::open(['url' => ['eliminar/ganado/:ID_ELIMINAR'], 'method' => 'DELETE','id'=>'form-delete']) !!}
+{!! Form::open(['url' => ['eliminar/muestra/:ID_ELIMINAR'], 'method' => 'DELETE','id'=>'form-delete']) !!}
 {!! Form::close() !!}
 
 @section('scripts')
--- resources/views/muestra/verMuestras.blade.php
+++ resources/views/muestra/verMuestras.blade.php
@@ -6,13 +6,11 @@
     @if (Auth::guest())
         @include('partials.permission')
     @else
-        <h1>Ganado</h1>
-        <p>Desde esta página puedes registrar una nueva res o editar las ya existentes y listadas.</p>
-        <a href="{{url('registrar/ganado')}}" class="btn btn-primary btn-md" role="button"><span class="glyphicon glyphicon-plus"></span> Nueva res</a>
-       {{-- <a href="{{route('verganadomuerto')}}" class="btn btn-danger btn-md" role="button"><span class="glyphicon glyphicon-minus"></span> Muertas</a>
-      --}}
-        <!-- TODO importar/exportar ganado -->
-        @include('ganado.tablaGanados')
+        <h1>Muestras</h1>
+        <p>Desde esta página puedes registrar una nueva muestra o editar las ya existentes y listadas.</p>
+        <a href="{{url('registrar/muestra')}}" class="btn btn-primary btn-md" role="button"><span class="glyphicon glyphicon-plus"></span> Nueva muestra</a>
+
+        @include('muestra.tablaMuestras')
 
     @endif
 @endsection
