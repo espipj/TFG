9ec576f7aca7746f665381f9313cd2e72f1cfad7
Pablo Espinosa <espipj@gmail.com>
2017-05-10 10:02:57 +0200

    added new users to create roles

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,5 +17,20 @@ class UsersTableSeeder extends Seeder
             'email' => 'admin',
             'password' => bcrypt('admin'),
         ]);
+        DB::table('users')->insert([
+            'name' => 'ganadero',
+            'email' => 'ganadero',
+            'password' => bcrypt('ganadero'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'asociacion',
+            'email' => 'asociacion',
+            'password' => bcrypt('asociacion'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'laboratorio',
+            'email' => 'laboratorio',
+            'password' => bcrypt('laboratorio'),
+        ]);
     }
 }
