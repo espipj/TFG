9fe86bd95b730ba2fc46f0e7fe6f9e7f9d8c1e29
Pablo Espinosa <espipj@gmail.com>
2017-05-20 15:26:30 +0200

    little changes in muestras

--- resources/views/muestra/verMuestra.blade.php
+++ resources/views/muestra/verMuestra.blade.php
@@ -12,9 +12,24 @@
             <h2>Res: <a href="{{route('vermuestra',[$muestra->ganado])}}">{{$muestra->ganado->crotal}}</a></h2>
             <h3>Tipo de muestra: {{$muestra->tipomuestra->nombre}}</h3>
             <h3>Tipo de consulta: {{$muestra->tipoconsulta->nombre}}</h3>
-            <h3>Laboratorio: <a href="{{route('verlaboratorio',[$muestra->laboratorio])}}">{{$muestra->laboratorio->nombre}}</a></h3>
+            <h3>Laboratorio: <a
+                        href="{{route('verlaboratorio',[$muestra->laboratorio])}}">{{$muestra->laboratorio->nombre}}</a>
+            </h3>
             <h3>Fecha de la extracción: {{$muestra->fecha_extraccion->format('d-m-Y')}}</h3>
 
+            @if(Auth::user()->hasAnyRole(array('Laboratorio')))
+                <a href="{{url('solicitar/padremadre',['muestra'=>$muestra])}}" class="btn btn-success btn-sm"
+                   role="button"><span
+                            class="glyphicon glyphicon-edit"></span> Filiar Padre y Madre</a>
+                <a href="{{url('solicitar/padremadre',['muestra'=>$muestra])}}" class="btn btn-success btn-sm"
+                   role="button"><span
+                            class="glyphicon glyphicon-edit"></span> Filiar Madre</a>
+                <a href="{{url('solicitar/padremadre',['muestra'=>$muestra])}}" class="btn btn-success btn-sm"
+                   role="button"><span
+                            class="glyphicon glyphicon-edit"></span> Filiar Padre</a>
+
+
+            @endif
         </div>
 
     @endif
