194ab64be4a1e05dfb2086703986ddddf49ebcdf
Pablo Espinosa <espipj@gmail.com>
2017-06-13 11:03:51 +0200

    Added main Panel

--- resources/views/home/admin.blade.php
+++ resources/views/home/admin.blade.php
@@ -10,3 +10,38 @@
 @if(Auth::user()->hasAnyRole(array('Laboratorio')))
     <a href="{{route('vermuestra')}}" role="button" class="btn btn-primary btn-lg" aria-pressed="true">Muestra</a>
 @endif
+
+<div class="row">
+@if(Auth::user()->hasAnyRole(array('Administrador','Laboratorio','Superadmin')))
+    <a href="{{url('ver/asociacion')}}"><div class="col-md-3 text-center cuadropan">
+            <img src="{{asset('images/asociacion.png')}}">
+            <h2>Asociación</h2>
+        </div></a>
+
+    <a href="{{url('ver/ganaderia')}}"><div class="col-md-3 text-center cuadropan">
+
+            <img src="{{asset('images/ganaderia.png')}}">
+            <h2>Ganadería</h2>
+        </div></a>
+@endif
+@if(Auth::user()->hasAnyRole(array('Administrador','Ganadero','Superadmin')))
+    <a href="{{url('ver/ganado')}}"><div class="col-md-3 text-center cuadropan">
+
+            <img src="{{asset('images/ganado.png')}}">
+            <h2>Ganado</h2>
+        </div></a>
+    <a href="{{route('verexplotacion')}}"><div class="col-md-3 text-center cuadropan">
+
+            <img src="{{asset('images/explotacion.png')}}">
+            <h2>Explotación</h2>
+        </div></a>
+@endif
+@if(Auth::user()->hasAnyRole(array('Laboratorio','Superadmin')))
+    <a href="{{route('vermuestra')}}"><div class="col-md-3 text-center cuadropan">
+
+            <img src="{{asset('images/muestra.png')}}">
+            <h2>Muestra</h2>
+        </div></a>
+@endif
+
+</div>
