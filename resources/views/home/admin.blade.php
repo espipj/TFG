5c9a21db990219b4d69dde0870347739e28ff919
Pablo Espinosa <espipj@gmail.com>
2017-06-14 22:34:40 +0200

    UI Changes

--- resources/views/home/admin.blade.php
+++ resources/views/home/admin.blade.php
@@ -1,4 +1,7 @@
 <div class="row">
+    <h1>Panel de Inicio:</h1>
+</div>
+<div class="row">
 @if(Auth::user()->hasAnyRole(array('Administrador','Laboratorio','Superadmin')))
     <a href="{{url('ver/asociacion')}}"><div class="col-md-3 text-center cuadropan">
             <img src="{{asset('images/asociacion.png')}}">
