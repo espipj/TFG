3d86055fa6b94dfb9a8ac5e680a356db7a391fc0
Pablo Espinosa <espipj@gmail.com>
2017-06-29 14:56:23 +0200

    Added error404 view

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,7 +4,13 @@
 @section('contenido')
 
    <div class="col-sm-6 col-sm-offset-3">
-       <h1>¡Error!</h1>
-       <h3>La pagina que andas buscando no existe.</h3>
+       <div class="card">
+           <div class="card-content">
+               <div class="card-title"><h1>¡Error!</h1></div>
+               <h3>La pagina que andas buscando no existe.</h3>
+           </div>
+       </div>
+
+
    </div>
 @endsection
