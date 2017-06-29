13560f351bd8a3bce799546f0862a978aadc9fa5
Pablo Espinosa <espipj@gmail.com>
2017-06-29 14:54:50 +0200

    Added error404 view

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+
+@section('contenido')
+
+   <div class="col-sm-6 col-sm-offset-3">
+       <h1>¡Error!</h1>
+       <h3>La pagina que andas buscando no existe.</h3>
+   </div>
+@endsection
