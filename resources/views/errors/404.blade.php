1fbe124f302f851ba529e2ea26cd51e214d030ce
Pablo Espinosa <espipj@gmail.com>
2017-06-29 15:08:42 +0200

    Added error404 view

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,9 +4,10 @@
 @section('contenido')
 
    <div class="col-sm-6 col-sm-offset-3">
-       <div class="card">
+       <div class="card" style="margin-bottom: 30px">
            <div class="card-content">
                <div class="card-title"><h1>¡Error!</h1></div>
+               <img src="{{asset('images/vaca_errorf.png')}}" style="width: 50%;"/>
                <h3>La pagina que andas buscando no existe.</h3>
            </div>
        </div>
