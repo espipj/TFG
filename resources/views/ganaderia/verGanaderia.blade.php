af9188c57cdeaca4c1645427ea313a0c12cdeec2
Pablo Espinosa <espipj@gmail.com>
2017-07-01 15:10:00 +0200

    UI Changes for Ganaderia

--- resources/views/ganaderia/verGanaderia.blade.php
+++ resources/views/ganaderia/verGanaderia.blade.php
@@ -5,37 +5,52 @@
     @if (Auth::guest())
         @include('partials.permission')
     @else
-    <div class="jumbotron">
-    <h1>Datos de la ganadería</h1>
-    <h2>Nombre: {{$ganaderia->nombre}}</h2>
-    <h2>Sigla: {{$ganaderia->sigla}}</h2>
-    <h2>Email: <a href="mailto:{{$ganaderia->email}}">{{$ganaderia->email}}</a></h2>
-    <h2>Telefono: <a href="tel:{{$ganaderia->telefono}}">{{$ganaderia->telefono}}</a></h2>
-    <h3>Asociación: {{$ganaderia->asociacion->nombre}} </h3>
-        <br>
+        <div class="row">
+            <div class="col-sm-6 col-sm-offset-3">
+                <div class="card">
+                    <div class="card-content">
+                    <span class="card-title">
+                        {{$ganaderia->nombre}}
+                    </span>
+                        <h3 class="card-color-text">Sigla: <div class="card-color-text-normal">{{$ganaderia->sigla}}</div></h3>
+                        <h4 class="card-color-text">Email: <div class="card-color-text-normal"><a href="mailto:{{$ganaderia->email}}">{{$ganaderia->email}}</a></div></h4>
+                        <h4 class="card-color-text">Telefono: <div class="card-color-text-normal"><a href="tel:{{$ganaderia->telefono}}">{{$ganaderia->telefono}}</a></div></h4>
+                        <h4 class="card-color-text">Asociación: <div class="card-color-text-normal">{{$ganaderia->asociacion->nombre}}</div></h4>
+                        <br>
+                        @if(Auth::user()->hasAnyRole(array('Administrador','SuperAdmin')))
+                            <div class="text-center">
+                                <a href="{{url('editar/ganaderia',['ganaderia'=>$ganaderia])}}"
+                                   class="btn btn-success btn-sm" role="button"><span
+                                            class="glyphicon glyphicon-edit"></span> Editar</a>
+
+                                <a href="" class="btn btn-danger btn-sm eliminar-detail"
+                                   role="button" data-id="{{$ganaderia->id}}"><span
+                                            class="glyphicon glyphicon-remove"></span> Eliminar</a>
+                            </div>
+                        @endif
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <h2>Ganado</h2>
         @if(Auth::user()->hasAnyRole(array('Administrador','SuperAdmin')))
-        <a href="{{url('editar/ganaderia',['ganaderia'=>$ganaderia])}}" class="btn btn-success btn-sm" role="button"><span class="glyphicon glyphicon-edit"></span> Editar</a>
-
-        <a href="" class="btn btn-danger btn-sm eliminar-detail"
-           role="button" data-id="{{$ganaderia->id}}"><span class="glyphicon glyphicon-remove"></span> Eliminar</a>
+            <a href="{{url('registrar/ganado',['ganaderia'=>$ganaderia])}}" class="btn btn-primary btn-sm"
+               role="button"><span class="glyphicon glyphicon-plus"></span> Registrar Ganado</a>
         @endif
-    </div>
-    <h2>Ganado</h2>
-    @if(Auth::user()->hasAnyRole(array('Administrador','SuperAdmin')))
-    <a href="{{url('registrar/ganado',['ganaderia'=>$ganaderia])}}" class="btn btn-primary btn-sm" role="button"><span class="glyphicon glyphicon-plus"></span> Registrar Ganado</a>
-    @endif
-    @include('ganado.tablaGanados')
+        @include('ganado.tablaGanados')
 
 
-    <h2>Explotaciones Ganaderas</h2>
-    @if(Auth::user()->hasAnyRole(array('Administrador','SuperAdmin')))
-    <a href="{{url('registrar/explotacion',['ganaderia'=>$ganaderia])}}" class="btn btn-primary btn-sm" role="button"><span class="glyphicon glyphicon-plus"></span> Registrar Explotación</a>
-    @endif
-    @include('explotacion.tablaExplotacion')
-    {{--<h2>Ganaderos</h2>
-    <a href="{{url('registrar/ganadero',['ganaderia'=>$ganaderia])}}" class="btn btn-primary btn-sm" role="button"><span class="glyphicon glyphicon-plus"></span> Registrar Ganadero</a>
-    @include('ganadero.tablaGanaderos')
---}}
+        <h2>Explotaciones Ganaderas</h2>
+        @if(Auth::user()->hasAnyRole(array('Administrador','SuperAdmin')))
+            <a href="{{url('registrar/explotacion',['ganaderia'=>$ganaderia])}}" class="btn btn-primary btn-sm"
+               role="button"><span class="glyphicon glyphicon-plus"></span> Registrar Explotación</a>
+        @endif
+        @include('explotacion.tablaExplotacion')
+        {{--<h2>Ganaderos</h2>
+        <a href="{{url('registrar/ganadero',['ganaderia'=>$ganaderia])}}" class="btn btn-primary btn-sm" role="button"><span class="glyphicon glyphicon-plus"></span> Registrar Ganadero</a>
+        @include('ganadero.tablaGanaderos')
+    --}}
     @endif
 @endsection
 
