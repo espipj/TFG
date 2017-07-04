75950b32d9079a91d2d577ad499d0396c59c3dd3
Pablo Espinosa <espipj@gmail.com>
2017-07-04 13:37:45 +0200

    Changes on carousel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,21 +1,18 @@
 @extends('layout')
 
 @section('contenido-fluid')
-
-
   <div class="carousel slide" data-ride="carousel" style="margin-top: -15px">
 
       <div class="carousel-inner">
           <div class="item active">
-              <img src="{{asset('images/vaca3.png')}}" class="img-responsive">
+              <img src="{{asset('images/vaca4.png')}}" class="img-responsive">
           </div>
           <div class="item">
-              <img src="{{asset('images/vaca2.png')}}" class="img-responsive">
+              <img src="{{asset('images/vaca5.png')}}" class="img-responsive">
           </div>
           <div class="item">
-              <img src="{{asset('images/vaca1.png')}}" class="img-responsive">
+              <img src="{{asset('images/vaca6.png')}}" class="img-responsive">
           </div>
       </div>
   </div>
-
 @endsection
